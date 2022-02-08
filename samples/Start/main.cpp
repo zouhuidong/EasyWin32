68b2bb5a105dc58580443f33c3497912f41b706c
huidong <61970628+zouhuidong@users.noreply.github.com>
2022-02-08 14:31:31 +0800

    Add files via upload

--- samples/Start/main.cpp
+++ samples/Start/main.cpp
@@ -9,7 +9,11 @@ int main()
 
 	FLUSH_DRAW();							// EasyWin32 默认使用双缓冲绘图，此处输出绘图缓冲
 
-	_getch();								// 按任意键继续
+	// 按任意键继续。注意：必须在此判断是否还存在窗口
+	while (!_kbhit() && EasyWin32::isAnyWindow())
+	{
+		Sleep(10);
+	}								
 
 	EasyWin32::closegraph_win32();			// 关闭窗口
 	return 0;
